aae290d69eeda6fb562664e4ecfdd1b9680efd63
Douglas Mogoba Nyamwaro <mogobanyamwaro@gmail.com>
2021-05-20 15:58:50 +0300

    Learned Strings for Java

---
+++ hello.java
@@ -0,0 +1,65 @@
+// import java.util.Date;
+
+// import jdk.internal.vm.compiler.word.Pointer;
+// import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+/**
+ * hello
+ */
+// public class hello {
+
+//     public static void main(String[] args) {
+//         int age = 30;
+//         int herAgee = age;
+//         System.out.println("hello world" + 30);
+//     }
+// }
+
+
+/**
+ * hello
+ */
+// public class hello {
+
+//     public static void main(String[] args) {
+//         byte age = 30;
+//         int viewsCount = 3_123_456_90;
+//         double price = 10.99;
+//         char letter = 'A';
+//         boolean isEligible = false;
+
+//     }
+// }
+
+/**
+ * hello
+ */
+// import java.util.Date;
+
+// public class hello {
+
+//    public static void main(String[] args) {
+//     byte age = 30;
+//     Date now = new Date();
+//     // now.getTime();
+    
+//     System.out.println(now);
+    
+//    }
+    
+// }
+
+/**
+ * hello
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        // String message = new String("hello world");
+        String message = "yolo";
+        System.out.println(message + " olo");
+        message.endsWith("eg");
+        System.out.println(message.indexOf("eg"));
+        
+    }
+}
